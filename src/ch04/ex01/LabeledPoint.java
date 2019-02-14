80ee3c40b48c491fa339706530bc5dfaee5c19e9
Kamil Piec <kamil062@gmail.com>
2019-02-14 01:02:21 +0100

    ch04ex01

--- src/ch04/ex01/LabeledPoint.java
+++ src/ch04/ex01/LabeledPoint.java
@@ -1,12 +1,5 @@
 package ch04.ex01;
 
-/*
-Zdefiniuj klasę Point z konstruktorem public Point(double x, double y) i metodami
-dostępowymi getX, getY. Zdefiniuj klasę podrzędną LabeledPoint z konstruktorem
-public LabeledPoint(String label, double x, double y) oraz metodę dostępową
-getLabel.
-*/
-
 public class LabeledPoint extends Point {
     private String label;
 
